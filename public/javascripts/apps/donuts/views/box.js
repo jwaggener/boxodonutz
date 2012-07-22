3da422b6a09b797518ae85f0cdef642fed0a9c3d
Johnny Waggener <john.waggener@gmail.com>
2012-07-22 18:32:47 -0400

    removing some unnecessary functions

--- public/javascripts/apps/donuts/views/box.js
+++ public/javascripts/apps/donuts/views/box.js
@@ -24,22 +24,8 @@ define([
 				this.$el.append( donutView.el );
 				i++;
 			}
-			//if there are 12 or less, use the template for a dozen donuts
-			//if there are more than 12, use the tmeplate for a hundred donuts
-		},
-		
-		renderDozen: function(){
-			
-		},
-		
-		renderHundred: function(){
-			
-		},
-		
-		//this shop only has boxes that accommodate a dozen or 100 donuts
-		setDonuts: function( donuts ){
-			
 		}
+		
 	});
 	
 	return Box;
